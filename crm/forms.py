2609bb1465fbc7bcca6fc6fbf1ad4ba580f868b0
pkanaganti <pkanaganti@unomaha.edu>
2018-09-17 09:27:26 -0500

    food service

---
+++ crm/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Customer, Service, Product
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('cust_name', 'organization', 'role', 'bldgroom', 'account_number', 'address', 'city', 'state', 'zipcode', 'email')
+
+class ServiceForm(forms.ModelForm):
+   class Meta:
+       model = Service
+       fields = ('cust_name', 'service_category', 'description', 'location', 'setup_time', 'cleanup_time', 'service_charge' )
+
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = ('cust_name', 'product', 'p_description', 'quantity', 'pickup_time', 'charge' )
